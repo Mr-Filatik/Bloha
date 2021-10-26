ff45547755fe5d3b0fdb8696a663a655b4591698
baikulov01 <79966062+baikulov01@users.noreply.github.com>
2021-10-26 12:46:37 +0400

    new bloha menu

---
+++ BlohaGame/Assets/Scripts/Scripts/MenuScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuScript : MonoBehaviour
+{
+    [SerializeField] private GameObject menuPanel = null; //main menu
+    [SerializeField] private GameObject shopPanel = null; //shop
+    [SerializeField] private GameObject barPanel = null; //bar with money
+    [SerializeField] private GameObject settingsPanel = null; //settings
+    [SerializeField] private GameObject infoPanel = null; //info
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
