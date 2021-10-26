a0f393a14748d98e19beb5eab130fc9d3198da07
baikulov01 <79966062+baikulov01@users.noreply.github.com>
2021-10-26 15:04:25 +0400

    new menu start end

--- BlohaGame/Assets/Scripts/Scripts/MenuScript.cs
+++ BlohaGame/Assets/Scripts/Scripts/MenuScript.cs
@@ -1,23 +1,57 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MenuScript : MonoBehaviour
 {
+    [SerializeField] private AnimationCurve panelAnimationX = null; //right - left
+    [SerializeField] private AnimationCurve panelAnimationY = null; //up - down
+    [SerializeField] private AnimationCurve panelAnimationScale = null; //scale
+    [SerializeField] private Vector3 panelStartPosition = Vector3.zero; //start position for panel
+    [SerializeField] private Vector3 panelEndPosition = Vector3.zero; //end position for panel
+
     [SerializeField] private GameObject menuPanel = null; //main menu
     [SerializeField] private GameObject shopPanel = null; //shop
     [SerializeField] private GameObject barPanel = null; //bar with money
     [SerializeField] private GameObject settingsPanel = null; //settings
     [SerializeField] private GameObject infoPanel = null; //info
+    [SerializeField] private GameObject gamePanel = null; //game
+    [SerializeField] private GameObject pausePanel = null; //pause
+    [SerializeField] private GameObject losingPanel = null; //losing
+    [SerializeField] private GameObject chancePanel = null; //chance
 
+    private void Awake()
+    {
+        //menuPanel.transform.localScale = Screen.width;
+        //Screen.width; Screen.height;
+    }
 
-    void Start()
+    private void Start()
     {
         
     }
 
-    void Update()
+    private void Update()
     {
         
+    }
+
+    private void SetCanvasScale()
+    {
+        CanvasScaler canvasScaler = gameObject.GetComponent<CanvasScaler>();
+        int widht = Screen.width;
+        if (widht > 720)
+        {
+            canvasScaler.scaleFactor = 1;
+        }
+        if (widht > 1080)
+        {
+            canvasScaler.scaleFactor = 1;
+        }
+        if (widht > 720)
+        {
+            canvasScaler.scaleFactor = 1;
+        }
     }
 }
